fc53f8cb72920c0c51490cde670b2293beddb021
Your Name <you@example.com>
2022-10-04 15:59:36 +0900

    Updates

--- gitbook/gitbook-plugin-bottom-navigation/plugin.js
+++ gitbook/gitbook-plugin-bottom-navigation/plugin.js
@@ -14,7 +14,7 @@ module.exports = function(book, page) {
     navigation.push('  </div>');
     navigation.push('  <div class="cardPreviousBody">');
     navigation.push('    <div class="cardHint">');
-    navigation.push('      <span>戻る</span>');
+    navigation.push('      <span>Previous</span>');
     navigation.push('    </div>');
     navigation.push('    <div class="cardTitle">');
     navigation.push(`      <span>${page.previous.title}</span>`);
@@ -27,7 +27,7 @@ module.exports = function(book, page) {
     navigation.push(`<a class="card cardNext">`);
     navigation.push('  <div class="cardNextBody">');
     navigation.push('    <div class="cardHint">');
-    navigation.push('      <span>次へ</span>');
+    navigation.push('      <span>Next</span>');
     navigation.push('    </div>');
     navigation.push('    <div class="cardTitle">');
     navigation.push(`      <span>${page.next.title}</span>`);
